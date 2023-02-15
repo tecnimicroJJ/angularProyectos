02e20a8c5fa5ba776a745ce4244aba352e63c2e5
tecnimicroJJ <tecnimicro@gmail.com>
2023-02-15 17:51:57 +0100

    aplicamos estilos a la tarjeta

---
+++ TarjetaCredito/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { CrearTarjetaComponent } from './components/crear-tarjeta/crear-tarjeta.component';
+import { ListarTarjetaComponent } from './components/listar-tarjeta/listar-tarjeta.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CrearTarjetaComponent,
+    ListarTarjetaComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ TarjetaCredito/src/app/components/crear-tarjeta/crear-tarjeta.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms'
+
+@Component({
+  selector: 'app-crear-tarjeta',
+  templateUrl: './crear-tarjeta.component.html',
+  styleUrls: ['./crear-tarjeta.component.css']
+})
+export class CrearTarjetaComponent {
+  form: FormGroup;
+
+
+  constructor(private fb:FormBuilder){
+    this.form = this.fb.group({
+      titular: ['',Validators.required],
+      numeroTarjeta: ['', [Validators.required,Validators.minLength(16),Validators.maxLength(16)]],
+      fechaExpiracion: ['', [Validators.required,Validators.minLength(5),Validators.maxLength(5)]],
+      cvv: ['',  [Validators.required,Validators.minLength(3),Validators.maxLength(3)]],
+    })
+  }
+
+  ngOnInit():void{
+  }
+
+  crearTarjeta(){
+    console.log(this.form);
+  }
+
+
+
+}
